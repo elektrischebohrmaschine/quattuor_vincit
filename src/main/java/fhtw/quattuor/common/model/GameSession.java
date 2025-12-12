2a7e7f8cc013189612404fbba4b395382b78831b
Daniel Drexler <ic24b038@technikum-wien.at>
2025-12-12 21:47:06 +0100

    Added Classes for Player and GameSession

---
+++ src/main/java/fhtw/quattuor/common/model/GameSession.java
@@ -0,0 +1,80 @@
+package fhtw.quattuor.common.model;
+
+
+public class GameSession {
+    int row = 5;
+    int col = 5;
+
+    private int[][] board;
+    private String opponent;
+    private int sessionNumber;
+    private boolean isFinished;
+    private boolean yourTurn;
+    private int moveCount;
+
+    public GameSession() {
+        this.board = new int[row][col];
+    }
+
+
+    public GameSession(String opponent, int sessionNumber, boolean yourTurn) {
+        this.board = new int[row][col];
+        this.opponent = opponent;
+        this.sessionNumber = sessionNumber;
+        this.yourTurn = yourTurn;
+        this.isFinished = false;
+        this.moveCount = 0;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public String getOpponent() {
+        return opponent;
+    }
+
+    public int getSessionNumber() {
+        return sessionNumber;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public boolean isYourTurn() {
+        return yourTurn;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void setFinished(boolean finished) {
+        isFinished = finished;
+    }
+
+    public void toggleTurn() {
+        yourTurn = !yourTurn;
+    }
+
+    public void setOpponent(String opponent) {
+        this.opponent = opponent;
+    }
+
+    public void setBoard(int[][] board) {
+        this.board = board;
+    }
+
+    public void setSessionNumber(int sessionNumber) {
+        this.sessionNumber = sessionNumber;
+    }
+
+    public void setYourTurn(boolean yourTurn) {
+        this.yourTurn = yourTurn;
+    }
+
+    public void setMoveCount(int moveCount) {
+        this.moveCount = moveCount;
+    }
+}
