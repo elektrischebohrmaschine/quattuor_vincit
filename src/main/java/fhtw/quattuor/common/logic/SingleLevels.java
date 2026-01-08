b9751866d6072c9a0a81fb5254e00609cde2bbe5
Maximilian Aigner <ic24b060@technikum-wien.at>
2026-01-08 16:18:38 +0100

    Added redrawing of Board (setAllColours). Added colorValidButton to color all buttons to be lightgray, otherwise white-ish. Added Reset of Buttons, after game reload.
    Seperated Colors to variables in Class ClientConnectFourGrid.
    Added Enemy Turns to Singleplayer Levels.

--- src/main/java/fhtw/quattuor/common/logic/SingleLevels.java
+++ src/main/java/fhtw/quattuor/common/logic/SingleLevels.java
@@ -4,63 +4,116 @@ import fhtw.quattuor.common.model.*;
 
 public class SingleLevels {
 
-    Board board;
-    int maxMoves;
+    private Board board;
+    private int maxMoves;
+    private int level_selected;
 
+    public SingleLevels(int level) {
+        level_selected = level;
+        switch (level) {
+            case 1:
+                Level1();
+                break;
+            case 2:
+                Level2();
+                break;
+            case 3:
+                Level3();
+                break;
+            default:
+                return;
+        }
+    }
 
-    public SingleLevels(Board board, int maxMoves) {
-        this.board = board;
+    private void Level1() {
+        board = new Board(6, 7);
+        board.setCellStatus(5, 6, CellStatus.PLAYER1);
+        board.setCellStatus(5, 4, CellStatus.PLAYER1);
+        board.setCellStatus(5, 3, CellStatus.PLAYER1);
+        board.setCellStatus(5, 2, CellStatus.PLAYER1);
+
+        board.setCellStatus(5, 5, CellStatus.PLAYER2);
+        board.setCellStatus(4, 4, CellStatus.PLAYER2);
+        board.setCellStatus(4, 3, CellStatus.PLAYER2);
+        board.setCellStatus(3, 3, CellStatus.PLAYER2);
+
+        maxMoves = 1;
+    }
+
+    private void Level2() {
+        board = new Board(6, 7);
+        board.setCellStatus(5, 5, CellStatus.PLAYER2);
+        board.setCellStatus(4, 5, CellStatus.PLAYER2);
+        board.setCellStatus(3, 5, CellStatus.PLAYER2);
+        board.setCellStatus(5, 4, CellStatus.PLAYER2);
+        board.setCellStatus(4, 4, CellStatus.PLAYER2);
+
+        board.setCellStatus(5, 2, CellStatus.PLAYER1);
+        board.setCellStatus(5, 3, CellStatus.PLAYER1);
+        board.setCellStatus(2, 5, CellStatus.PLAYER1);
+        board.setCellStatus(4, 3, CellStatus.PLAYER1);
+
+        maxMoves = 1;
+    }
+
+    private void Level3() {
+        board = new Board(6, 7);
+        board.setCellStatus(5, 4, CellStatus.PLAYER1);
+        board.setCellStatus(4, 1, CellStatus.PLAYER1);
+        board.setCellStatus(4, 3, CellStatus.PLAYER1);
+        board.setCellStatus(4, 4, CellStatus.PLAYER1);
+        board.setCellStatus(4, 5, CellStatus.PLAYER1);
+        board.setCellStatus(3, 1, CellStatus.PLAYER1);
+        board.setCellStatus(2, 3, CellStatus.PLAYER2);
+        board.setCellStatus(5, 2, CellStatus.PLAYER1);
+
+        board.setCellStatus(5, 1, CellStatus.PLAYER2);
+        board.setCellStatus(5, 3, CellStatus.PLAYER2);
+        board.setCellStatus(3, 3, CellStatus.PLAYER2);
+        board.setCellStatus(5, 5, CellStatus.PLAYER2);
+        board.setCellStatus(3, 4, CellStatus.PLAYER2);
+        board.setCellStatus(1, 1, CellStatus.PLAYER2);
+        board.setCellStatus(4, 2, CellStatus.PLAYER2);
+        board.setCellStatus(2, 1, CellStatus.PLAYER2);
+
+        maxMoves = 2;
+    }
+
+    public Board enemyTurn(Board board) {
+        switch (level_selected) {
+            case 1:
+                board.setCellStatus(5, 1, CellStatus.PLAYER2);
+                break;
+            case 2:
+                board.setCellStatus(3, 4, CellStatus.PLAYER2);
+                break;
+            case 3:
+                if (maxMoves == 1) {
+                    if (board.getCellStatus(2, 4) == CellStatus.EMPTY) {
+                        board.setCellStatus(2, 4, CellStatus.PLAYER2);
+                    } else {
+                        board.setCellStatus(5, 6, CellStatus.PLAYER2);
+                    }
+                } else {
+                    board.setCellStatus(4, 6, CellStatus.PLAYER2);
+                }
+                break;
+            default:
+                break;
+        }
+
+        return board;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public int getMaxMoves() {
+        return maxMoves;
+    }
+
+    public void setMaxMoves(int maxMoves) {
         this.maxMoves = maxMoves;
-    }
-
-    public static SingleLevels Level1(Board board) {
-        board.setCellStatus(5,6,CellStatus.PLAYER1);
-        board.setCellStatus(5,4,CellStatus.PLAYER1);
-        board.setCellStatus(5,3,CellStatus.PLAYER1);
-        board.setCellStatus(5,2,CellStatus.PLAYER1);
-
-        board.setCellStatus(5,5,CellStatus.PLAYER2);
-        board.setCellStatus(4,4,CellStatus.PLAYER2);
-        board.setCellStatus(4,3,CellStatus.PLAYER2);
-        board.setCellStatus(3,3,CellStatus.PLAYER2);
-
-        return new SingleLevels(board,1);
-    }
-
-    public static SingleLevels Level2(Board board) {
-        board.setCellStatus(5,5,CellStatus.PLAYER2);
-        board.setCellStatus(4,5,CellStatus.PLAYER2);
-        board.setCellStatus(3,5,CellStatus.PLAYER2);
-        board.setCellStatus(5,4,CellStatus.PLAYER2);
-        board.setCellStatus(4,4,CellStatus.PLAYER2);
-
-        board.setCellStatus(5,2,CellStatus.PLAYER1);
-        board.setCellStatus(5,3,CellStatus.PLAYER1);
-        board.setCellStatus(2,5,CellStatus.PLAYER1);
-        board.setCellStatus(4,3,CellStatus.PLAYER1);
-
-        return new SingleLevels(board,1);
-    }
-    public static SingleLevels Level3(Board board) {
-        board.setCellStatus(5,4,CellStatus.PLAYER1);
-        board.setCellStatus(4,1,CellStatus.PLAYER1);
-        board.setCellStatus(4,3,CellStatus.PLAYER1);
-        board.setCellStatus(4,4,CellStatus.PLAYER1);
-        board.setCellStatus(4,5,CellStatus.PLAYER1);
-        board.setCellStatus(3,1,CellStatus.PLAYER1);
-        board.setCellStatus(2,1,CellStatus.PLAYER1);
-        board.setCellStatus(5,2,CellStatus.PLAYER1);
-
-        board.setCellStatus(5,1,CellStatus.PLAYER2);
-        board.setCellStatus(5,3,CellStatus.PLAYER2);
-        board.setCellStatus(3,3,CellStatus.PLAYER2);
-        board.setCellStatus(5,5,CellStatus.PLAYER2);
-        board.setCellStatus(3,4,CellStatus.PLAYER2);
-        board.setCellStatus(2,3,CellStatus.PLAYER2);
-        board.setCellStatus(1,1,CellStatus.PLAYER2);
-        board.setCellStatus(4,2,CellStatus.PLAYER2);
-
-
-        return new SingleLevels(board,1);
     }
 }
